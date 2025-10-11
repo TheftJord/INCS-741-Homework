21854767019b5b3fe52511fabea284c71559dc06
TheftJord <155499388+TheftJord@users.noreply.github.com>
2025-10-11 13:06:50 -0400

    Major Update
    I made it so you can use multiple hashes at once and that each one would be working on a thread reducing the time needed for the mass decoding

--- main.java
+++ main.java
@@ -5,72 +5,78 @@ import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.concurrent.Executor;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class main {
 
     // Universal Variables
+
+    // The alphabet put into an ArrayList so I can use it in for loops
     static ArrayList<String> letters = new ArrayList<>(Arrays.asList("A","B","C","D","E",
     "F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"));
 
+    //-----------------------------------------------Primary Method------------------------------------------------
     
-    /**"
+    /**
      * This is the class that starts the code
+     * @param argc
+     * @throws NoSuchAlgorithmException
+     * @throws InterruptedException
      */
-    public static void main(String[] argc) throws NoSuchAlgorithmException{
+    public static void main(String[] argc) throws NoSuchAlgorithmException, InterruptedException{
         primaryFunction();
     }
 
     /**
-     * This is the class that calls the other methods that will be used to solve the problem
-     * The Methods called are the following:
-     * 
+     * The Class that is primary used to crack the hashes
+     * This is required for repeat()
+     * @throws NoSuchAlgorithmException
+     * @throws InterruptedException
      */
-    public static void primaryFunction() throws NoSuchAlgorithmException{
+    public static void primaryFunction() throws NoSuchAlgorithmException, InterruptedException{
         // variables
-        String liscenseNumber = null;
+        String [] hashes;
+        String shaValue;
 
         // functions
         Scanner keyboard = new Scanner(System.in);
-        ExecutorService threads = Executors.newCachedThreadPool(null);
 
-        // getting hash
+        // getting hashs
         System.out.print("Please Input SHA-256 Value: ");
-        String shaValue = keyboard.next();
+        shaValue = keyboard.next();
+
+        // Seperating Hashes
+        hashes = shaValue.split(",");
+
+        // Making the Thread Pools
+        ExecutorService threads = Executors.newFixedThreadPool(hashes.length); //makes threads
+        CountDownLatch lock = new CountDownLatch(hashes.length); // makes locks
 
         // --- Problem Solving Code ---
 
-        for(int x = 0; x < 26; x++){
-            for(int y = 0; y < 26; y++){
-                for(int z = 0; z < 26; z++){
-                    for(int num1 = 0; num1 < 10; num1++){
-                        for(int num2 = 0; num2 < 10; num2++){
-                            for(int num3 = 0; num3 < 10; num3++){
-                                for (int num4 = 0; num4 < 10; num4++){
-                                    String tempString = letters.get(x) + letters.get(y) + letters.get(z) 
-                                    + "-" + Integer.toString(num1) + Integer.toString(num2) + 
-                                    Integer.toString(num3) + Integer.toString(num4);
-                                    String tempHash = hexString(getSha(tempString));
-                                    if(shaValue.compareTo(tempHash)==0){
-                                        liscenseNumber = tempString;
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                    }
+        System.out.println("The Liscense Plate Numbers are: ");
+        for(String part : hashes){ // loops for each hash
+            threads.execute(() -> { //creates threads for each hash to reduce time
+                try {
+                    String solution = algorithm(part); // sends thread to cracking algo
+                    System.out.println("- " + solution); // prints solution
+                } catch (NoSuchAlgorithmException ex) {
+                    System.out.println("An Error has Occured");
                 }
-            } //});
+                lock.countDown(); // decreases countdown on the lock
+            });
         }
 
-            System.out.println("The Liscense Plate is " + liscenseNumber);
+        lock.await(); // makes program wait for all the threads to be finished
 
         // --- Problem Solving Code ---
 
-        repeat();
+        repeat(); // allows user to repeat code without restarting
     }
+
+    //------------------------------------------Hash Cracking Methods----------------------------------------------
 
     /**
      * This converts the binary that we got from getSha into a string that we are able to use
@@ -78,14 +84,19 @@ public class main {
      * @return
      */
     public static String hexString(byte[] hash){
+
+        // converts byte[] into a BigInteger so it is in one variable
         BigInteger binary = new BigInteger(1, hash);
 
+        // Transforms binary to a string
         StringBuilder hexString = new StringBuilder(binary.toString(16));
         
+        // Makes sure that there are enough characters
         while(hexString.length() < 64){
             hexString.insert(0,'0');
         }
 
+        // fixes format and returns hash
         return "0x" + hexString.toString().toUpperCase();
     }
 
@@ -97,12 +108,67 @@ public class main {
      * @throws NoSuchAlgorithmException
      */
     public static byte[] getSha(String inputtedString) throws NoSuchAlgorithmException{
+
+        // the encoder that will trans for the binary to hashed binary
         MessageDigest encoder = MessageDigest.getInstance("SHA-256");
 
+        // hashes and returns the binary
         return encoder.digest(inputtedString.getBytes(StandardCharsets.UTF_8));
     }
 
-    public static void repeat() throws NoSuchAlgorithmException{
+    /**
+     * This is used to find the unhashed version of the liscense plate
+     * @param result
+     * @return
+     * @throws NoSuchAlgorithmException
+     */
+    public static String algorithm(String result) throws NoSuchAlgorithmException{
+
+        // variables
+        String returnValue = null;
+
+        // Cracking Loops
+        for(int x = 0; x < 26; x++){ // Goes through slot 1
+            for(int y = 0; y < 26; y++){ // Goes throught slot 2
+                for(int z = 0; z < 26; z++){ // Goes throught slot 3
+                    for(int num1 = 0; num1 < 10; num1++){ // Goes throught slot 4
+                        for(int num2 = 0; num2 < 10; num2++){ // Goes throught slot 5
+                            for(int num3 = 0; num3 < 10; num3++){ // Goes throught slot 6
+                                for (int num4 = 0; num4 < 10; num4++){ // Goes throught slot 7
+
+                                    // converts the present values into a liscense plate number
+                                    String tempString = letters.get(x) + letters.get(y) + letters.get(z) 
+                                    + "-" + Integer.toString(num1) + Integer.toString(num2) + 
+                                    Integer.toString(num3) + Integer.toString(num4);
+
+                                    // hashes the liscense plate
+                                    String tempHash = hexString(getSha(tempString));
+
+                                    // Compares the results of the hashed liscense plate
+                                    if(result.compareTo(tempHash)==0){ //checks if they are the same
+                                        returnValue = tempString; // if true will set the returnValue to match
+                                        break; // breaks the loop
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Returns found un-hashed value
+        return returnValue;
+    }
+
+    //------------------------------------------------MISC----------------------------------------------------------
+
+    /**
+     * This allows the user to repeat the code without restarting the entire program
+     * @throws NoSuchAlgorithmException
+     * @throws InterruptedException
+     */
+    public static void repeat() throws NoSuchAlgorithmException, InterruptedException{
        
         // functions
         Scanner keyboard = new Scanner(System.in);
@@ -113,7 +179,7 @@ public class main {
 
         // process response
         if(response.compareTo("Y")==0 || response.compareTo("y")==0){
-            /* main(); */ System.out.println("fix this");
+            primaryFunction();
         }
         else if(response.compareTo("N")==0 || response.compareTo("n")==0){
             System.out.println("Thank you for your service");
@@ -122,32 +188,4 @@ public class main {
             System.out.println("Improper Response; Please respond with a Y or a N");
         }
     }
-
-    public static String algorithm(String result) throws NoSuchAlgorithmException{
-        String returnValue = null;
-        for(int x = 0; x < 26; x++){
-            for(int y = 0; y < 26; y++){
-                for(int z = 0; z < 26; z++){
-                    for(int num1 = 0; num1 < 10; num1++){
-                        for(int num2 = 0; num2 < 10; num2++){
-                            for(int num3 = 0; num3 < 10; num3++){
-                                for (int num4 = 0; num4 < 10; num4++){
-                                    String tempString = letters.get(x) + letters.get(y) + letters.get(z) 
-                                    + "-" + Integer.toString(num1) + Integer.toString(num2) + 
-                                    Integer.toString(num3) + Integer.toString(num4);
-                                    String tempHash = hexString(getSha(tempString));
-                                    if(result.compareTo(tempHash)==0){
-                                        returnValue = tempString;
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return returnValue;
-    }
-
 }
